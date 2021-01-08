5af6a8990c057039fd54866e126c086a013a915a
Narayan Jha <52191223+cgqspider@users.noreply.github.com>
2021-01-09 00:15:38 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var cron = require('node-cron');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var port = process.env.PORT || 3000;
+ 
+
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+
+  cron.schedule('* * * * *', function() {
+   io.emit("messageSent","Hello");
+    console.log('running a task every minute');
+  });
+
+  io.on('connection', function(client) {
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+    });
+
+    client.on('messages', function(data) {
+           client.emit('broad', data);
+           client.broadcast.emit('broad',data);
+    });
+
+});
+  
+
+
+  server.listen(3000);
